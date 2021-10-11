284df8c136cc1b4c66aa719c1f2821b6f328814d
Mickael F <mickael.frotey@email.fr>
2021-10-11 14:55:26 +0200

    slugs & comments

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -48,8 +48,10 @@ class BlogController extends AbstractController
 
             $this->addFlash('success', 'Article publié avec succès !');
 
-            // TODO: Changer la redirection vers la page de l'article créé
-            return $this->redirectToRoute('main_home');
+
+            return $this->redirectToRoute('blog_publication_view', [
+                'slug' => $newArticle->getSlug(),
+            ]);
 
         }
 
@@ -57,4 +59,35 @@ class BlogController extends AbstractController
             'form' => $form->createView(),
         ]);
     }
+
+    /**
+     * Page qui liste tous les articles
+     *
+     * @Route("/publications/liste/", name="publication_list")
+     */
+    public function publicationList(): Response
+    {
+
+        $articleRepo = $this->getDoctrine()->getRepository(Article::class);
+
+        $articles = $articleRepo->findAll();
+
+        return $this->render('blog/publicationList.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+
+    /**
+     * Page permettant de voir un article en détail
+     *
+     * @Route("/publication/{slug}/", name="publication_view")
+     */
+    public function publicationView(Article $article): Response
+    {
+
+        return $this->render('blog/publicationView.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
 }
