28c57b361bf89d9e0b943a7c59695b9ff1bd49f1
Mickael F <mickael.frotey@email.fr>
2021-10-11 14:25:39 +0200

    clone pour la correction

---
+++ src/Controller/MainController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * Contrôleur de la page d'accueil
+     *
+     * @Route("/", name="main_home")
+     */
+    public function home(): Response
+    {
+        return $this->render('main/home.html.twig');
+    }
+
+    /**
+     * Page de profil
+     *
+     * @Route("/mon-profil/", name="main_profil")
+     * @Security("is_granted('ROLE_USER')")
+     */
+    public function profil(): Response
+    {
+
+        return $this->render('main/profil.html.twig');
+    }
+
+}
